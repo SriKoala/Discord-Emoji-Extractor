d5114b92f2c44655bddb5f283a9214a09d8e2b60
SPKunda <sripranav.kunda@gmail.com>
2020-10-25 15:08:46 -0700

    Added source code links

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ function getImage(id)
     if (jpg.checked) format = "jpg";
     if (gif.checked) format = "gif";
     
-    $("#result").html(`<img src="https://cdn.discordapp.com/emojis/${id}.${format}" /> <br><br> <a href="https://cdn.discordapp.com/emojis/${id}.${format}" class="btn btn-primary">Open in new tab</a><br><br>`);
+    $("#result").html(`<img src="https://cdn.discordapp.com/emojis/${id}.${format}" /> <br><br> <a href="https://cdn.discordapp.com/emojis/${id}.${format}" class="btn btn-primary" target="_blank">Open in new tab</a><br><br>`);
 }
