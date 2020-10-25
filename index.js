06f12ed33116e299b2ac0c2df0b8d3578ea7a450
SriPKunda <sripranav.kunda@gmail.com>
2020-10-25 14:23:14 -0700

    Finished

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 function getImage(id)
 {
-    $.get(`https://cdn.discordapp.com/emojis/${id}.png`, function( data ) {
-        $( ".result" ).html( data );
-        alert( "Load was performed." );
-      });
+    var format; 
+
+    if (png.checked) format = "png";
+    if (jpg.checked) format = "jpg";
+    if (gif.checked) format = "gif";
+    
+    $("#result").html(`<img src="https://cdn.discordapp.com/emojis/${id}.${format}" /> <br><br> <a href="https://cdn.discordapp.com/emojis/${id}.${format}" class="btn btn-primary">Open in new tab</a><br><br>`);
 }
